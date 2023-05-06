cfddd6a8a209151fb61a606ac93f652a3236426c
CoolingY <1848062200@qq.com>
2023-05-06 18:10:07 +0800

    初步搭建

---
+++ Blog/src/main/java/com/BlogApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.mapper")
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class,args);
+    }
+}
---
+++ Blog/src/main/java/com/controller/ArticleController.java
@@ -0,0 +1,27 @@
+package com.controller;
+
+import com.domain.Article;
+import com.domain.ResponseResult;
+import com.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+
+    @GetMapping("/hotArticleList")
+    public ResponseResult hotArticleList() {
+
+        ResponseResult result = articleService.hotArticleList();
+        return result;
+    }
+}
