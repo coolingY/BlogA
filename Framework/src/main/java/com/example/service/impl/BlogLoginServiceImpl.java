08cfbb519b55771722af5951bc83f761e5d0bc83
coolingY <108736570+coolingY@users.noreply.github.com>
2023-05-22 15:19:24 +0800

    Add Hello,GitHub!

--- Framework/src/main/java/com/example/service/impl/BlogLoginServiceImpl.java
+++ Framework/src/main/java/com/example/service/impl/BlogLoginServiceImpl.java
@@ -30,6 +30,7 @@ public class BlogLoginServiceImpl implements BlogLoginService {
     public ResponseResult login(User user) {
 
         System.out.println("Hello,IDEA!");
+        System.out.println("Hello,GitHub!");
         UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
         if(Objects.isNull(authenticate)){
