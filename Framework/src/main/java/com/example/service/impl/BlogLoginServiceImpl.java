53faa38398a49af62ec75be70238334cdd1d9af1
CoolingY <1848062200@qq.com>
2023-05-23 14:09:17 +0800

    Restart

--- Framework/src/main/java/com/example/service/impl/BlogLoginServiceImpl.java
+++ Framework/src/main/java/com/example/service/impl/BlogLoginServiceImpl.java
@@ -29,8 +29,7 @@ public class BlogLoginServiceImpl implements BlogLoginService {
     @Override
     public ResponseResult login(User user) {
 
-        System.out.println("ABC");
-        System.out.println("C");
+
         UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(user.getUserName(),user.getPassword());
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
         if(Objects.isNull(authenticate)){
